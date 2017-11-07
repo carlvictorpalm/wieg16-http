7b27363b36d7acf078d76154d3aa87fab073719c
Victor Palm <carlvictorpalm@gmail.com>
2017-11-07 11:35:33 +0100

    function

--- functions.php
+++ functions.php
@@ -1,6 +1,15 @@
 <?php
 // "HTTP/1.0 404 Not Found"
-function status_header($code = 200) {}
+function status_header($code = 200) {
+	$messages = [
+		200 => "OK",
+		500 => "Server Error"		
+	];
+	header("HTTP/1.0 ".$code. " ".$messages[$code]);
+
+
+}
+status_header();
 
 // [header => värde]
 function headers(array $headers = []) {}
